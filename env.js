4453649eb789f7b5183997794eb9d3f5d8dc915d
unfollowedDev <92226269+unfollowedDev@users.noreply.github.com>
2020-05-26 19:46:05 +0000

    Bump version to 1.1

--- env.js
+++ env.js
@@ -15,4 +15,11 @@ const TESTNET = {
     APP_NAME: 'Teloscan (testnet)',
     NETWORK_HOST: 'testnet.telos.net',
     NETWORK_CHAIN_ID:
-      '1ea
+      '1eaa0824707c8c16bd25145493bf062aecddfeb56c736f6ba6397f3195f33c9f',
+    NETWORK_EVM_RPC: 'https://testnet.telos.net/evm',
+    NETWORK_EVM_ENDPOINT: 'https://testnet.telos.net',
+    NETWORK_EVM_CHAIN_ID: 41,
+    HYPERION_ENDPOINT: 'https://testnet.telos.net',
+    NETWORK_EXPLORER: 'https://explorer-test.telos.net',
+    TELOS_API_ENDPOINT: 'https://api-dev.telos.net/v1', //'http://localhost:9999/v1', //for local instance of api
+    VERI
