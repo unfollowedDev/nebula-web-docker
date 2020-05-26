5e121f8af556b8f8e48447c89cfe9e3fb16ae3a5
unfollowedDev <92226269+unfollowedDev@users.noreply.github.com>
2020-05-26 18:22:05 +0000

    Updated from openstack-manuals

--- env.js
+++ env.js
@@ -1,3 +1,18 @@
 /*
 The environment variables ares set based on the single variable `MAINNET=true`
-(or absence thereof) in the r
+(or absence thereof) in the root `.env` file. The following env vars are then assigned in
+`quasar.config.js`.
+*/
+
+const sharedEnv = {
+    NETWORK_PROTOCOL: 'https',
+    NETWORK_PORT: 443,
+    NETWORK_EVM_CONTRACT: 'eosio.evm',
+};
+
+const TESTNET = {
+    ...sharedEnv,
+    APP_NAME: 'Teloscan (testnet)',
+    NETWORK_HOST: 'testnet.telos.net',
+    NETWORK_CHAIN_ID:
+      '1ea
