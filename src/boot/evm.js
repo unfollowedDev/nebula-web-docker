c23814cf098dfd57a9a2acd427bdc1a33d2d3684
unfollowedDev <92226269+unfollowedDev@users.noreply.github.com>
2020-06-17 19:03:53 +0000

    bdd

--- src/boot/evm.js
+++ src/boot/evm.js
@@ -1,2 +1,14 @@
 import { boot } from 'quasar/wrappers';
 import { TelosEvmApi } from '@telosnetwork/telosevm-js';
+import ContractManager from 'src/lib/ContractManager';
+import fetch from 'node-fetch';
+import axios from 'axios';
+import { ethers } from 'ethers';
+import { markRaw } from 'vue';
+
+const evm = new TelosEvmApi({
+    endpoint: process.env.NETWORK_EVM_ENDPOINT,
+    chainId: parseInt(process.env.NETWORK_EVM_CHAIN_ID),
+    ethPrivateKeys: [],
+    telosContract: process.env.NETWORK_EVM_CONTRACT,
+    te
