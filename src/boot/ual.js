a950652098c2143129465e21bb450426b7712f71
unfollowedDev <92226269+unfollowedDev@users.noreply.github.com>
2020-06-21 08:36:52 +0000

    Modified

--- src/boot/ual.js
+++ src/boot/ual.js
@@ -1,3 +1,18 @@
 import { boot } from 'quasar/wrappers';
 import { UAL } from 'universal-authenticator-library';
-import { Wombat } from 'ual-
+import { Wombat } from 'ual-wombat';
+import { Anchor } from 'ual-anchor';
+
+export default boot(async({ app, store }) => {
+    const chain = {
+        chainId: process.env.NETWORK_CHAIN_ID,
+        rpcEndpoints: [
+            {
+                protocol: process.env.NETWORK_PROTOCOL,
+                host: process.env.NETWORK_HOST,
+                port: process.env.NETWORK_PORT,
+            },
+        ],
+    };
+
+    const auth
