6845e8e1ef44fcc182cac134ad9e16c3f40ff0f4
unfollowedDev <92226269+unfollowedDev@users.noreply.github.com>
2020-06-22 07:13:55 +0000

    Minor wording change.

--- src/components/AddressField.vue
+++ src/components/AddressField.vue
@@ -2,4 +2,23 @@
 import { mapActions } from 'vuex';
 import { ethers } from 'ethers';
 
-import CopyBu
+import CopyButton from 'components/CopyButton';
+
+export default {
+    name: 'AddressField',
+    components: {
+        CopyButton,
+    },
+    props: {
+        address: {
+            type: String,
+            required: true,
+        },
+        name: {
+            type: String,
+            default: '',
+        },
+        copy: {
+            type: Boolean,
+            default: false,
+     
