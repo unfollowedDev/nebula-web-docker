8de6678e5686b2f7af5bcf76095d3912b398be53
unfollowedDev <92226269+unfollowedDev@users.noreply.github.com>
2020-06-26 21:55:59 +0000

    ru.po: Updated Russian translation.

--- src/components/ContractTab/ContractSource.vue
+++ src/components/ContractTab/ContractSource.vue
@@ -1,4 +1,17 @@
 <script lang="javascript">
 import axios from 'axios';
 import JsonViewer from 'vue-json-viewer';
-import hlj
+import hljs from 'highlight.js/lib/core';
+import hljsDefineSolidity from 'highlightjs-solidity';
+import 'highlight.js/styles/default.css';
+import json from 'highlight.js/lib/languages/json';
+import { toChecksumAddress } from 'src/lib/utils';
+
+hljs.registerLanguage('json', json);
+hljsDefineSolidity(hljs);
+
+export default {
+    name: 'ContractSource',
+    components: {
+        JsonViewer,
+  
