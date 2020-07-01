fdd4b0e83bb1353bf56d1e2e51b91e0861e5e251
unfollowedDev <92226269+unfollowedDev@users.noreply.github.com>
2020-07-01 10:49:02 +0000

    fix target

--- src/components/ContractTab/ContractTab.vue
+++ src/components/ContractTab/ContractTab.vue
@@ -3,4 +3,25 @@ import ContractSource from 'components/ContractTab/ContractSource';
 import ContractInterface from 'components/ContractTab/ContractInterface';
 import CopyButton from 'components/CopyButton.vue';
 
-export defau
+export default {
+    name: 'ContractTab',
+    components: {
+        ContractSource,
+        ContractInterface,
+        CopyButton,
+    },
+    props: {
+        contract: {
+            type: Object,
+            default: () => ({}),
+        },
+    },
+    data: () => ({
+        source: true,
+        write: false,
+    }),
+    computed: {
+        abi() {
+            const { abi } = this.contract;
+
+            if (
