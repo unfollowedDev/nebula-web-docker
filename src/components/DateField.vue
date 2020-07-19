72eff6457b3f090cfd458ebbfeca7853772b9f23
unfollowedDev <92226269+unfollowedDev@users.noreply.github.com>
2020-07-19 10:19:55 +0000

    update Travis

--- src/components/DateField.vue
+++ src/components/DateField.vue
@@ -10,4 +10,11 @@ export default {
             type: Number,
             required: true,
         },
-        // if true, the component will display plain-english
+        // if true, the component will display plain-english e.g. "3 minutes ago" on initial load; if false, it will
+        // display long format e.g. "Jan 6, 2023 11:41:40 (UTC -05:00)"
+        defaultToAge: {
+            type: Boolean,
+            default: true,
+        },
+        // a value of true or false will force the component to display plain-english age or date stamp respectively;
+        // the default value of null will allow the component to toggle its
