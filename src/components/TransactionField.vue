be38bc7ef83f507f3bad5da49492d58af2031f21
unfollowedDev <92226269+unfollowedDev@users.noreply.github.com>
2020-07-22 19:30:52 +0000

    fix font

--- src/components/TransactionField.vue
+++ src/components/TransactionField.vue
@@ -6,3 +6,20 @@ export default {
     components:{
         CopyButton,
     },
+    props: {
+        transactionHash: {
+            type: String,
+            required: false,
+        },
+        copy: {
+            type: Boolean,
+            default: false,
+        },
+    },
+};
+</script>
+
+<template>
+<div class="transaction-field-container">
+    <router-link :key="$route.path" :to="`/tx/${this.transactionHash}`">
+        {{ transactionHash && transactionHash.slic
