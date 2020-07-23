8abfdf4aca218a179b2e2eccefe52cb462924064
unfollowedDev <92226269+unfollowedDev@users.noreply.github.com>
2020-07-23 13:43:47 +0000

    0.2.11

--- src/components/TransactionTable.vue
+++ src/components/TransactionTable.vue
@@ -136,3 +136,201 @@ export default {
                 this.pagination.page = Number(page);
             }
             if (size) {
+                this.pagination.rowsPerPage = Number(size);
+            }
+            this.onRequest({
+                pagination: this.pagination,
+            });
+        },
+        async onPaginationChange(props) {
+            const { page, rowsPerPage } = props.pagination;
+
+            // we need to change the URL to keep the pagination state by changing the this.$route.query.page
+            // with a string like 'page,rowsPerPage'
+            this.$router.push({
+                // taking care to preserve the current #hash anchor and the current query parameters
+                hash: window.location.hash,
+                query: {
+                    ...this.$route.query,
+                    page: `${page},${rowsPerPage}`,
+                },
+            });
+        },
+        async onRequest(props) {
+            this.loading = true;
+
+            const { page, rowsPerPage, sortBy, descending } = props.pagination;
+            let result = await this.$evmEndpoint.get(this.getPath(props));
+
+            if (this.total === null) {
+                this.pagination.rowsNumber = result.data.total.value;
+            }
+
+            this.pagination.page = page;
+            this.pagination.rowsPerPage = rowsPerPage;
+            this.pagination.sortBy = sortBy;
+            this.pagination.descending = descending;
+            this.transactions = result.data.transactions;
+            for (const transaction of this.transactions) {
+                try {
+                    transaction.transfer = false;
+                    transaction.value = formatWei(transaction.value.toLocaleString(0, { useGrouping: false }), 18);
+                    if (transaction.input_data === '0x') {
+                        continue;
+                    }
+                    if(!transaction.to) {
+                        continue;
+                    }
+
+                    const contract = await this.$contractManager.getContract(
+                        transaction.to,
+                    );
+
+                    if (!contract) {
+                        continue;
+                    }
+
+                    const parsedTransaction = await contract.parseTransaction(
+                        transaction.input_data,
+                    );
+                    if (parsedTransaction) {
+                        transaction.parsedTransaction = parsedTransaction;
+                        transaction.contract = contract;
+                    }
+                    // Get ERC20 transfer from main function call
+                    let signature = transaction.input_data.substring(0, 10);
+                    if (
+                        signature &&
+                        TRANSFER_SIGNATURES.includes(signature) &&
+                        transaction.parsedTransaction.args['amount']
+                    ) {
+                        let token = await this.$contractManager.getTokenData(transaction.to, 'erc20');
+                        if(transaction.contract && token && token.decimals){
+                            transaction.transfer = {
+                                'value': `${formatWei(transaction.parsedTransaction.args['amount'], token.decimals)}`,
+                                'symbol': token.symbol,
+                            };
+                        }
+                    }
+                } catch (e) {
+                    console.error(
+                        `Failed to parse data for transaction, error was: ${e.message}`,
+                    );
+                    // notifiy user
+                    this.$q.notify({
+                        message: this.$t('components.failed_to_parse_transaction', { message: e.message }),
+                        color: 'negative',
+                        position: 'top',
+                        timeout: 5000,
+                    });
+                }
+            }
+            this.rows = this.transactions;
+            this.loading = false;
+        },
+        getPath(props) {
+            const { page, rowsPerPage, descending } = props.pagination;
+            let path = `/v2/evm/get_transactions?limit=${
+                rowsPerPage === 0 ? 500 : rowsPerPage
+            }`;
+            const filter = Object.assign({}, this.filter ? this.filter : {});
+            if (filter.address) {
+                path += `&address=${filter.address}`;
+            }
+
+            if (filter.block) {
+                path += `&block=${filter.block}`;
+            }
+
+            if (filter.hash) {
+                path += `&hash=${filter.hash}`;
+            }
+
+            path += `&skip=${(page - 1) * rowsPerPage}`;
+            path += `&sort=${descending ? 'desc' : 'asc'}`;
+
+            return path;
+        },
+        toggleDateFormat() {
+            this.showDateAge = !this.showDateAge;
+        },
+    },
+};
+</script>
+
+<template>
+<q-table
+    v-model:pagination="pagination"
+    :rows="rows"
+    :row-key="row => row.hash"
+    :columns="columns"
+    :loading="loading"
+    :rows-per-page-options="page_size_options"
+    flat
+    @request="onPaginationChange"
+>
+    <template v-slot:header="props">
+        <q-tr :props="props">
+            <q-th v-for="col in props.cols" :key="col.name" :props="props">
+                <div class="u-flex--center-y">
+                    {{ col.label }}
+                    <template v-if="col.name === 'date'">
+                        <q-icon
+                            class="info-icon"
+                            name="fas fa-info-circle"
+                            @click="toggleDateFormat"
+                        >
+                            <q-tooltip anchor="bottom middle" self="bottom middle" :offset="[0, 36]">
+                                {{ $t('components.click_to_change_format') }}
+                            </q-tooltip>
+                        </q-icon>
+                    </template>
+                    <template v-if="col.name === 'method'">
+                        <q-icon class="info-icon" name="fas fa-info-circle" />
+                        <q-tooltip anchor="bottom middle" self="top middle" max-width="10rem">
+                            {{ $t('components.executed_based_on_decoded_data') }}
+                        </q-tooltip>
+                    </template>
+                </div>
+            </q-th>
+        </q-tr>
+    </template>
+    <template v-slot:body="props">
+        <q-tr :props="props">
+            <q-td key="hash" :props="props">
+                <TransactionField :transaction-hash="props.row.hash"/>
+            </q-td>
+            <q-td key="block" :props="props">
+                <BlockField :block="props.row.block"/>
+            </q-td>
+            <q-td key="date" :props="props">
+                <DateField :epoch="props.row.epoch" :force-show-age="showDateAge"/>
+            </q-td>
+            <q-td key="method" :props="props">
+                <MethodField v-if="props.row.parsedTransaction" :trx="props.row" :shortenName="true"/>
+            </q-td>
+            <q-td key="from" :props="props">
+                <AddressField v-if="props.row.from" :address="props.row.from"/>
+            </q-td>
+            <q-td key="to" :props="props">
+                <AddressField
+                    v-if="props.row.to"
+                    :key="props.row.to + ((props.row.contract) ? '1' : '0')"
+                    :address="props.row.to"
+                    :isContractTrx="!!(props.row.contract)"
+                />
+            </q-td>
+            <q-td key="value" :props="props">
+                <span v-if="props.row.value > 0 ||  !props.row.transfer ">
+                    {{ props.row.value }} TLOS
+                </span>
+                <div v-else>
+                    <span v-if="props.row.transfer">
+                        {{ props.row.transfer.value }} {{ props.row.transfer.symbol }}
+                    </span>
+                </div>
+            </q-td>
+        </q-tr>
+    </template>
+</q-table>
+</template>
---
+++ src/components/TransferTable.vue
@@ -0,0 +1,5 @@
+<script>
+import AddressField from 'components/AddressField';
+import DateField from 'components/DateField';
+import TransactionField from 'components/TransactionField';
+import { ethers, BigNumber } f
