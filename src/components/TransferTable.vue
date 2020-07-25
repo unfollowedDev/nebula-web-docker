682ec9c21d1bfa430e40ba61a2eb5397bbbae12e
unfollowedDev <92226269+unfollowedDev@users.noreply.github.com>
2020-07-25 11:28:43 +0000

    lmao

--- src/components/TransferTable.vue
+++ src/components/TransferTable.vue
@@ -8,4 +8,20 @@ import DEFAULT_TOKEN_LOGO from 'src/assets/evm_logo.png';
 import { TRANSFER_SIGNATURES } from 'src/lib/abi/signature/transfer_signatures';
 
 const TRANSFER_EVENT_ERC20_SIGNATURE = '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef';
-const TRANSFER_EVENT_ERC1155_SIGNATURE = '0xc3d58168c5ae7397731
+const TRANSFER_EVENT_ERC1155_SIGNATURE = '0xc3d58168c5ae7397731d063d5bbf3d657854427343f4c083240f7aacaa2d0f62';
+const TOKEN_ID_TRUNCATE_LENGTH = 66;
+
+export default {
+    name: 'TransferTable',
+    components: {
+        TransactionField,
+        DateField,
+        AddressField,
+    },
+    props: {
+        title: {
+            type: String,
+            required: true,
+        },
+        tokenType: {
+            type: Stri
