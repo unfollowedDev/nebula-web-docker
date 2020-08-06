ce940504fdd623f7552053591211fd4082fa8e42
unfollowedDev <92226269+unfollowedDev@users.noreply.github.com>
2020-08-06 21:37:12 +0000

    Funceble Test - Autosave

--- src/components/header/LanguageSwitcherModal.vue
+++ src/components/header/LanguageSwitcherModal.vue
@@ -18,4 +18,18 @@ export default {
     created() {
         this.selectedLanguage = {
             code: this.$i18n.locale,
-            name: this.$t('locale.current_langu
+            name: this.$t('locale.current_language_name'),
+        };
+
+        // languageOptions: [
+        //     { code: 'en-us', name: 'English'},
+        //     { code: 'es-es', name: 'Español'},
+        //     ...
+        // ],
+        this.languageOptions = Object.keys(messages).map(key => ({
+            code: key,
+            name: messages[key].locale.current_language_name,
+        }));
+    },
+    watch: {
+        'select
