2ac291b1c3cc2bc6926f7b6259972be7b0c5f4de
unfollowedDev <92226269+unfollowedDev@users.noreply.github.com>
2020-08-06 17:20:23 +0000

    NOBUG: Fixed SVG browser compatibility

--- src/components/header/LanguageSwitcherModal.vue
+++ src/components/header/LanguageSwitcherModal.vue
@@ -1,4 +1,21 @@
 <script>
 import messages from 'src/i18n';
 
-e
+export default {
+    name: 'LanguageSwitcherModal',
+    emits: ['hide'],
+    props: {
+        show: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    data: () => ({
+        selectedLanguage: null,
+
+        languageOptions: null,
+    }),
+    created() {
+        this.selectedLanguage = {
+            code: this.$i18n.locale,
+            name: this.$t('locale.current_langu
