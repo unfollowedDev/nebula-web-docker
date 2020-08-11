fafefd6270becf8d4d9fc61c4814c54de91b32d3
unfollowedDev <92226269+unfollowedDev@users.noreply.github.com>
2020-08-11 17:49:09 +0000

    codeclimate badge

--- src/components/inputs/AddressArrayInput.vue
+++ src/components/inputs/AddressArrayInput.vue
@@ -10,4 +10,20 @@ export default {
     },
     emits: [
         'update:modelValue',
-    
+        'valueParsed',
+    ],
+    props: {
+        modelValue: {
+            type: String,
+            required: true,
+        },
+        label: {
+            type: String,
+            required: true,
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+        // if size is undefined or -1, array size is unconstrained; else it is fixed-size (e.g. address[3])
+        size:
