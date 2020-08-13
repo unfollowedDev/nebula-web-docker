edba3b80bb31178282d9d0af27a22b7b4459e5bb
unfollowedDev <92226269+unfollowedDev@users.noreply.github.com>
2020-08-13 17:34:43 +0000

    Moving files around.

---
+++ src/components/inputs/BooleanInput.vue
@@ -0,0 +1,10 @@
+<script>
+export default {
+    name: 'BooleanInput',
+    emits: [
+        'update:modelValue',
+    ],
+    props: {
+        modelValue: {
+            type: Boolean,
+            default: nul
