f90b2e58edebdd8414f6605bbe1bdaceb61e2125
unfollowedDev <92226269+unfollowedDev@users.noreply.github.com>
2020-08-16 13:44:51 +0000

    fixed #16

--- src/components/inputs/StringInput.vue
+++ src/components/inputs/StringInput.vue
@@ -1,2 +1,21 @@
 <script>
-import BaseTextInput
+import BaseTextInput from 'components/inputs/BaseTextInput';
+
+export default {
+    name: 'StringInput',
+    components: {
+        BaseTextInput,
+    },
+    emits: [
+        'update:modelValue',
+    ],
+    props: {
+        modelValue: {
+            type: String,
+            required: true,
+        },
+        label: {
+            type: String,
+            required: true,
+        },
+ 
