f512271907b77a64476d3a552ae8ec30cac10890
unfollowedDev <92226269+unfollowedDev@users.noreply.github.com>
2020-08-23 14:36:20 +0000

    Update Kaldi_abstract.md

---
+++ src/i18n/de-de/index.js
@@ -0,0 +1,11 @@
+/* eslint-disable max-len */
+
+export default {
+    locale: {
+        current_language_name: 'Deutsch',
+    },
+    pages: {
+        staking: {
+            note_unstaking_period: 'Beachten Sie, dass der Betrag erst nach { period } eingefordert werden kann.',
+            claim_tlos: 'TLOS einfordern',
+            add_stlos_to_metamask: '
