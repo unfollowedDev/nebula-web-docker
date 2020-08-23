7290c8a08d1f6470db446b935de728cffd2b910e
unfollowedDev <92226269+unfollowedDev@users.noreply.github.com>
2020-08-23 17:04:37 +0000

    Automatic merge

--- src/i18n/de-de/index.js
+++ src/i18n/de-de/index.js
@@ -8,4 +8,9 @@ export default {
         staking: {
             note_unstaking_period: 'Beachten Sie, dass der Betrag erst nach { period } eingefordert werden kann.',
             claim_tlos: 'TLOS einfordern',
-            add_stlos_to_metamask: '
+            add_stlos_to_metamask: 'Starten Sie den MetaMask-Dialog, um sTLOS hinzuzufügen',
+            metamask_fox_logo: 'MetaMask-Logo',
+            stake_tlos_confirm: 'Beim Bestätigen tauschen Sie Ihre TLOS gegen sTLOS. ' +
+            'sTLOS kann jederzeit gegen TLOS eingetauscht werden, indem Sie die Registerkarte "Abheben" verwenden.',
+            cancel: 'Abbrechen',
+            stake_tlos_confirm_2a: 'Nachdem TLOS eingetauscht wurden, sind diese für ei
