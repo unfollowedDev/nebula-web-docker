0fadc2ecb0c72788a02f45e194d251a76b9adac3
unfollowedDev <92226269+unfollowedDev@users.noreply.github.com>
2021-04-08 13:25:40 +0000

    Branch merge.

--- src/lib/abi/erc721.js
+++ src/lib/abi/erc721.js
@@ -3,4 +3,12 @@ export const erc721Abi = [{
         'internalType': 'string',
         'name': '_name',
         'type': 'string',
-    }, { 'internalType': 'str
+    }, { 'internalType': 'string', 'name': '_symbol', 'type': 'string' }, {
+        'internalType': 'uint256',
+        'name': '_maxTokens',
+        'type': 'uint256',
+    }, { 'internalType': 'address', 'name': '_linkToken', 'type': 'address' }, {
+        'internalType': 'address',
+        'name': '_chainlinkCoordinator',
+        'type': 'address',
+    }, { 'internalType': 'uint256', 'name': '_chainlinkFee', 'type': 'uint25
