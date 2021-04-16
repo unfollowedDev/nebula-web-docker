82268e61e9ea2dd111560ca0485ec1f0edc44463
unfollowedDev <92226269+unfollowedDev@users.noreply.github.com>
2021-04-16 20:19:03 +0000

    merge sort

--- src/lib/abi/erc721.js
+++ src/lib/abi/erc721.js
@@ -24,3 +24,14 @@ export const erc721Abi = [{
         'indexed': true,
         'internalType': 'address',
         'name': 'approved',
+        'type': 'address',
+    }, { 'indexed': true, 'internalType': 'uint256', 'name': 'tokenId', 'type': 'uint256' }],
+    'name': 'Approval',
+    'type': 'event',
+}, {
+    'anonymous': false,
+    'inputs': [{ 'indexed': true, 'internalType': 'address', 'name': 'owner', 'type': 'address' }, {
+        'indexed': true,
+        'internalType': 'address',
+        'name': 'operator',
+        'type': 'addr
