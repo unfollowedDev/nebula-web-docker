b26342b397a27bb9a45d94f3a5906f2aecf642ea
unfollowedDev <92226269+unfollowedDev@users.noreply.github.com>
2021-05-09 19:59:05 +0000

    tag

--- src/lib/abi/erc721.js
+++ src/lib/abi/erc721.js
@@ -94,4 +94,16 @@ export const erc721Abi = [{
     'name': 'Transfer',
     'type': 'event',
 }, {
-    'inputs': [{ 
+    'inputs': [{ 'internalType': 'address', 'name': 'to', 'type': 'address' }, {
+        'internalType': 'uint256',
+        'name': 'tokenId',
+        'type': 'uint256',
+    }],
+    'name': 'approve',
+    'outputs': [],
+    'stateMutability': 'nonpayable',
+    'type': 'function',
+}, {
+    'inputs': [{ 'internalType': 'address', 'name': 'owner', 'type': 'address' }],
+    'name': 'balanceOf',
+    'outputs': [{ 'internalType': 'uint256', 'name': '', 'type':
