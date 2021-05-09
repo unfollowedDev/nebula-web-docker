71d76959e40e1c5ac39e96b3fa12496a4158ae52
unfollowedDev <92226269+unfollowedDev@users.noreply.github.com>
2021-05-09 17:31:07 +0000

    tracing: Reset ftrace_graph_filter_enabled if count is zero

--- src/lib/abi/erc721.js
+++ src/lib/abi/erc721.js
@@ -82,3 +82,16 @@ export const erc721Abi = [{
         'type': 'bytes32',
     }],
     'name': 'SetRandomSeed',
+    'type': 'event',
+}, {
+    'anonymous': false,
+    'inputs': [{ 'indexed': true, 'internalType': 'address', 'name': 'from', 'type': 'address' }, {
+        'indexed': true,
+        'internalType': 'address',
+        'name': 'to',
+        'type': 'address',
+    }, { 'indexed': true, 'internalType': 'uint256', 'name': 'tokenId', 'type': 'uint256' }],
+    'name': 'Transfer',
+    'type': 'event',
+}, {
+    'inputs': [{ 
