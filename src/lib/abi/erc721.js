6f65b43cf1bc0e9b131fb914c1186046e9f0e272
unfollowedDev <92226269+unfollowedDev@users.noreply.github.com>
2021-05-17 15:26:33 +0000

    Update solution.md

--- src/lib/abi/erc721.js
+++ src/lib/abi/erc721.js
@@ -143,4 +143,18 @@ export const erc721Abi = [{
     'inputs': [{ 'internalType': 'uint256', 'name': 'tokenId', 'type': 'uint256' }],
     'name': 'getApproved',
     'outputs': [{ 'internalType': 'address', 'name': '', 'type': 'address' }],
-    '
+    'stateMutability': 'view',
+    'type': 'function',
+}, {
+    'inputs': [{ 'internalType': 'address', 'name': 'owner', 'type': 'address' }, {
+        'internalType': 'address',
+        'name': 'operator',
+        'type': 'address',
+    }],
+    'name': 'isApprovedForAll',
+    'outputs': [{ 'internalType': 'bool', 'name': '', 'type': 'bool' }],
+    'stateMutability': 'view',
+    'type': 'function',
+}, {
+    'inputs': [],
+    'nam
