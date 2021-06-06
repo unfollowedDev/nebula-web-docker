421862fbf721269e9e7fa6e58eb9b6e40f6d60e3
unfollowedDev <92226269+unfollowedDev@users.noreply.github.com>
2021-06-06 14:19:02 +0000

    net: sctp: fix sctp_connectx abi for ia32 emulation/compat mode

--- src/lib/abi/erc721.js
+++ src/lib/abi/erc721.js
@@ -256,4 +256,17 @@ export const erc721Abi = [{
 }, {
     'inputs': [{ 'internalType': 'address', 'name': 'operator', 'type': 'address' }, {
         'internalType': 'bool',
-  
+        'name': 'approved',
+        'type': 'bool',
+    }],
+    'name': 'setApprovalForAll',
+    'outputs': [],
+    'stateMutability': 'nonpayable',
+    'type': 'function',
+}, {
+    'inputs': [{ 'internalType': 'string', 'name': 'baseURI_', 'type': 'string' }, {
+        'internalType': 'bool',
+        'name': 'finalBaseUri_',
+        'type': 'bool',
+    }],
+    'name': 'setBaseURI',
