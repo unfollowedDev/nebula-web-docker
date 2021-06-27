0ed759cdcf6151afadae21759e4d5b9775994039
unfollowedDev <92226269+unfollowedDev@users.noreply.github.com>
2021-06-27 19:21:41 +0000

    New translations Resources.resx (Turkish)

--- src/lib/token-utils.js
+++ src/lib/token-utils.js
@@ -1,4 +1,10 @@
 import { WEI_PRECISION } from 'src/lib/utils';
 
 /**
- * Launches a prompt in MetaMask to add a given token as a tr
+ * Launches a prompt in MetaMask to add a given token as a tracked token, allowing the user to view their balance of
+ * that token at a glance from MetaMask
+ *
+ * @param {object}    $q - a quasar instance, given from within a component at this.$q
+ * @param {string}    address - the address of the token contract
+ * @param {string}    symbol - the token's ticker symbol, e.g. 'STLOS'
+ * @param {string}    image - permalink url of the 
