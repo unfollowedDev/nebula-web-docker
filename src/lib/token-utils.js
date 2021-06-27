109438dfcf3bda27274a26968b5b5ea0df56aae2
unfollowedDev <92226269+unfollowedDev@users.noreply.github.com>
2021-06-27 19:41:49 +0000

    Fixed code formatting.

--- src/lib/token-utils.js
+++ src/lib/token-utils.js
@@ -7,4 +7,15 @@ import { WEI_PRECISION } from 'src/lib/utils';
  * @param {object}    $q - a quasar instance, given from within a component at this.$q
  * @param {string}    address - the address of the token contract
  * @param {string}    symbol - the token's ticker symbol, e.g. 'STLOS'
- * @param {string}    image - permalink url of the 
+ * @param {string}    image - permalink url of the token's icon
+ * @param {string}    type - Ethereum standard of the token; default is 'ERC20'
+ * @param {number}    decimals - the number of decimals constituting the token's precision, default is 18
+ *
+ * @returns {Promise<boolean>} - true iff the user granted the request to track sTLOS
+ */
+export async function promptAddToMetamask(
+    $q,
+    address,
+    symbol,
+    image,
+    type = 'ERC20',
