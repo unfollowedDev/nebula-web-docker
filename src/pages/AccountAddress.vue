e71cdfa9bdc45db636405ca2a6b175e6a80f9cb8
unfollowedDev <92226269+unfollowedDev@users.noreply.github.com>
2021-07-02 15:16:08 +0000

    msm: vidc: Fix session error handling

--- src/pages/AccountAddress.vue
+++ src/pages/AccountAddress.vue
@@ -1,3 +1,10 @@
 <!-- eslint-disable vue/no-unused-components -->
 <script>
-import { toChecksumAddress 
+import { toChecksumAddress } from 'src/lib/utils';
+import Web3 from 'web3';
+import TransactionTable from 'components/TransactionTable';
+import InternalTransactionTable from 'components/InternalTransactionTable';
+import TransferTable from 'components/TransferTable';
+import TokenList from 'components/TokenList';
+import ConfirmationDialog from 'components/ConfirmationDialog';
+import ContractTab from 'components/ContractTab/Contra
