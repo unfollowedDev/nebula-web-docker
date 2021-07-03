3a819a0644b40b1835303bec28b01d7d47e574e0
unfollowedDev <92226269+unfollowedDev@users.noreply.github.com>
2021-07-03 15:57:37 +0000

    baby steps

--- src/pages/AccountAddress.vue
+++ src/pages/AccountAddress.vue
@@ -7,4 +7,14 @@ import InternalTransactionTable from 'components/InternalTransactionTable';
 import TransferTable from 'components/TransferTable';
 import TokenList from 'components/TokenList';
 import ConfirmationDialog from 'components/ConfirmationDialog';
-import ContractTab from 'components/ContractTab/Contra
+import ContractTab from 'components/ContractTab/ContractTab';
+import TransactionField from 'components/TransactionField';
+import AddressField from 'components/AddressField';
+import CopyButton from 'components/CopyButton';
+import GenericContractInterface from 'components/ContractTab/GenericContractInterface.vue';
+
+const web3 = new Web3();
+
+const tabs = {
+    transactions: '#transactions',
+    int_transactions: '#int_transactions',
