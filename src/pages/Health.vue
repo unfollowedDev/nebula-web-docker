38837d4062bce838f82d325bb451f82641bd378c
unfollowedDev <92226269+unfollowedDev@users.noreply.github.com>
2021-08-26 20:55:15 +0000

    initial project commit

---
+++ src/pages/Health.vue
@@ -0,0 +1,11 @@
+<script>
+import Endpoints from 'components/Health/Endpoints.vue';
+import Monitor from 'components/Health/Monitor.vue';
+export default {
+    name: 'HealthPage',
+    components: { Endpoints, Monitor },
+};
+</script>
+
+<template>
+<div class="pageContainer q-pt-x
