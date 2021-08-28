90ed694f65764db469b28ff58e03f05375d82d75
unfollowedDev <92226269+unfollowedDev@users.noreply.github.com>
2021-08-28 20:57:27 +0000

    Ooops.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,2 +1,18 @@
 <script>
-import TransactionTable from 'component
+import TransactionTable from 'components/TransactionTable';
+import HomeInfo from 'components/HomeInfo.vue';
+export default {
+    name: 'IndexPage',
+    components: {
+        HomeInfo,
+        TransactionTable,
+    },
+};
+</script>
+
+<template>
+<q-page class="pageContainer">
+    <div class="row justify-center q-py-lg">
+        <div class="text-h4 text-primary title q-mt-xl bold">{{ $t('pages.telos_evm_explorer') }}</div>
+    </div>
+    <div cla
