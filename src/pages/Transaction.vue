d426fc02829bb15724b0bab797a004a9b613fa7a
unfollowedDev <92226269+unfollowedDev@users.noreply.github.com>
2021-08-29 10:37:09 +0000

    Bla

--- src/pages/Transaction.vue
+++ src/pages/Transaction.vue
@@ -1,2 +1,10 @@
 <script>
-import { mapGetters, mapActions } from 'vue
+import { mapGetters, mapActions } from 'vuex';
+import DateField from 'components/DateField';
+import BlockField from 'components/BlockField';
+import AddressField from 'components/AddressField';
+import LogsViewer from 'components/Transaction/LogsViewer';
+import InternalTxns from 'components/Transaction/InternalTxns';
+import CopyButton from 'components/CopyButton';
+import MethodField from 'components/MethodField';
+import ERCTransferList from 'components/Transaction/
