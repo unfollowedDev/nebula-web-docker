a2e31cfb8f714406056a2232524c62b53a8a6756
unfollowedDev <92226269+unfollowedDev@users.noreply.github.com>
2021-12-19 20:24:56 +0000

    Update readme links

--- src/pages/demo/DemoIndexPage.vue
+++ src/pages/demo/DemoIndexPage.vue
@@ -6,3 +6,18 @@ export default {
             this.$q.dark.toggle();
             localStorage.setItem('darkModeEnabled', this.$q.dark.isActive);
         },
+    },
+};
+</script>
+
+<template>
+<div class="c-demos">
+    <div class="row q-mb-sm">
+        <div class="col u-flex--left text-white">
+            <div class="text-h3">Component Demos</div>
+            <q-btn
+                flat
+                standout
+                class="q-mx-md"
+                :icon="$q.dark.isActive ? 'fas fa-sun' : 'fas fa-moon'"
+          
