4bdfdd97b18cc84e9d803e92a0701c44bfd7defa
unfollowedDev <92226269+unfollowedDev@users.noreply.github.com>
2021-12-25 09:45:28 +0000

    Bump version: 0.0.4 → 0.0.5

--- src/pages/demo/inputs/InputsDemo.vue
+++ src/pages/demo/inputs/InputsDemo.vue
@@ -124,3 +124,162 @@ export default {
                         </option>
                     </select>
                     <label for="signedIntSizeOptions">&nbsp;Bits for <code>int</code>-based inputs</label>
+                    <br>
+
+                    <select id="unsignedIntSizeOptions" v-model="selectedUnsignedIntSizeOption">
+                        <option
+                            v-for="option in unsignedIntSizeOptions"
+                            :key="option"
+                            :value="option"
+                        >
+                            {{ option }}
+                        </option>
+                    </select>
+                    <label for="allArraysFixedSize">&nbsp;Bits for <code>uint</code>-based inputs</label>
+                    <br>
+                </div>
+            </div>
+
+            <div class="row q-mb-lg">
+                <div class="col-12">
+                    <p>Vanilla inputs</p>
+                </div>
+                <div class="col-xs-12 col-md-6 col-lg-3 col-xl-2">
+                    <BaseTextInput
+                        v-model="baseTextInputValue"
+                        v-bind="universalToggles"
+                        label="Base Text Input"
+                        name="base input"
+                    />
+                </div>
+            </div>
+            <div class="row q-mb-lg">
+                <div class="col-12">
+                    <p>Contract interface inputs</p>
+                </div>
+                <div class="col-xs-12 col-md-6 col-lg-3 col-xl-2">
+                    <StringInput
+                        v-model="stringInputValue"
+                        v-bind="universalToggles"
+                        label="String Input"
+                        name="string input"
+                    />
+                </div>
+
+                <div class="col-xs-12 col-md-6 col-lg-3 col-xl-2">
+                    <UnsignedIntInput
+                        v-model="uintInputValue"
+                        v-bind="universalToggles"
+                        label="Unsigned Integer Input"
+                        name="uint input"
+                        :size="selectedUnsignedIntSizeOption"
+                    />
+                </div>
+
+                <div class="col-xs-12 col-md-6 col-lg-3 col-xl-2">
+                    <SignedIntInput
+                        v-model="intInputValue"
+                        v-bind="universalToggles"
+                        label="Signed Integer Input"
+                        name="int input"
+                        :size="selectedSignedIntSizeOption"
+                    />
+                </div>
+
+                <div class="col-xs-12 col-md-6 col-lg-3 col-xl-2">
+                    <AddressInput
+                        v-model="addressInputValue"
+                        v-bind="universalToggles"
+                        label="Address Input"
+                        name="address input"
+                    />
+                </div>
+
+                <div class="col-xs-12 col-md-6 col-lg-3 col-xl-2">
+                    <BooleanInput
+                        v-model="booleanInputValue"
+                        v-bind="universalToggles"
+                        label="Boolean Input"
+                        name="bool input"
+                    />
+                </div>
+
+                <div class="col-xs-12 col-md-6 col-lg-3 col-xl-2">
+                    <BytesArrayInput
+                        v-model="bytesArrayInputValue"
+                        v-bind="universalToggles"
+                        label="Bytes Input"
+                        name="bytes array input"
+                        size="2"
+                    />
+                </div>
+
+                <div class="col-xs-12 col-md-6 col-lg-3 col-xl-2">
+                    <BytesArrayInput
+                        v-model="unconstrainedBytesArrayInputValue"
+                        v-bind="universalToggles"
+                        label="Byte Array Input"
+                        name="unconstrained bytes array input"
+                    />
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-12">
+                    <p>Contract interface inputs (Arrays)</p>
+                </div>
+                <div class="col-xs-12 col-md-6 col-lg-3 col-xl-2">
+                    <StringArrayInput
+                        v-model="stringArrayInputValue"
+                        v-bind="arrayToggles"
+                        label="String Array Input"
+                        name="string array input"
+                    />
+                </div>
+
+                <div class="col-xs-12 col-md-6 col-lg-3 col-xl-2">
+                    <AddressArrayInput
+                        v-model="addressArrayInputValue"
+                        v-bind="arrayToggles"
+                        label="Address Array Input"
+                        name="address array input"
+                    />
+                </div>
+
+                <div class="col-xs-12 col-md-6 col-lg-3 col-xl-2">
+                    <UnsignedIntArrayInput
+                        v-model="uintArrayInputValue"
+                        v-bind="arrayToggles"
+                        label="Unsigned Int Array Input"
+                        name="uint array input"
+                        :uint-size="selectedUnsignedIntSizeOption"
+                    />
+                </div>
+
+                <div class="col-xs-12 col-md-6 col-lg-3 col-xl-2">
+                    <SignedIntArrayInput
+                        v-model="signedIntArrayInputValue"
+                        v-bind="arrayToggles"
+                        label="Signed Int Array Input"
+                        name="int array input"
+                        :int-size="selectedSignedIntSizeOption"
+                    />
+                </div>
+
+                <div class="col-xs-12 col-md-6 col-lg-3 col-xl-2">
+                    <BooleanArrayInput
+                        v-model="boolArrayInputValue"
+                        v-bind="arrayToggles"
+                        label="Boolean Array Input"
+                        name="bool array input"
+                    />
+                </div>
+            </div>
+        </div>
+    </q-card>
+</div>
+</template>
+
+<style scoped>
+
+</style>
