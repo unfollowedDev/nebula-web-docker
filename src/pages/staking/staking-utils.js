4e0b4181cdb5776804570d71f1df95ea76ad775d
unfollowedDev <92226269+unfollowedDev@users.noreply.github.com>
2022-04-09 22:33:50 +0000

    Corrections mineures

--- src/pages/staking/staking-utils.js
+++ src/pages/staking/staking-utils.js
@@ -2,4 +2,20 @@ const DAY_SECONDS = 86400;
 const HOUR_SECONDS = 3600;
 
 /**
- * Translates a number of seconds to a plain-e
+ * Translates a number of seconds to a plain-english string, eg. 90 => "1.5 minutes"
+ *
+ * @param {number|null} seconds
+ * @returns {string} plain english time period
+ */
+export function formatUnstakePeriod(seconds, $t) {
+    if (seconds === null) {
+        return '--';
+    }
+
+    let quantity;
+    let unit;
+
+    if (seconds < HOUR_SECONDS) {
+        quantity = seconds / 60;
+        unit = $t('pages.staking.minutes');
+    } 
