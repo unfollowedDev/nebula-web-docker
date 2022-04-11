a56a63d2b1c592f984a0dad34159e022fcbd1967
unfollowedDev <92226269+unfollowedDev@users.noreply.github.com>
2022-04-11 16:58:11 +0000

    актуализация

--- src/router/routes.js
+++ src/router/routes.js
@@ -7,4 +7,16 @@ const routes = [
                 path: '',
                 name: 'home',
                 component: () => import('pages/Index.vue'),
-         
+            },
+        ],
+    },
+    {
+        path: '/address/:address',
+        component: () => import('layouts/MainLayout.vue'),
+        children: [
+            {
+                path: '',
+                name: 'address',
+                props: route => ({ page: route.query.page, pagesize: route.query.pagesize }),
+                component: () => import('pages/AccountAddress.vue'),
+ 
