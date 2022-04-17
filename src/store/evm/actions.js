4d5314c214cbd3e84cb5a18eabff53e227ac1162
unfollowedDev <92226269+unfollowedDev@users.noreply.github.com>
2022-04-17 07:39:32 +0000

    major version bump

--- src/store/evm/actions.js
+++ src/store/evm/actions.js
@@ -9,4 +9,17 @@ export async function doRPC(_, { method, params }) {
         method,
         params,
     };
-    const result = await this.$evmEndpoint.post('/ev
+    const result = await this.$evmEndpoint.post('/evm', rpcPayload);
+
+    return result.data;
+}
+
+// TODO: make sure we're not using this and remove...
+//  should be using the $contractManager that's on prototype (this.$contractManager)
+export async function getContract(_, { address }) {
+    return await this.$contractManager.getContract(address);
+}
+
+export const fetchTlosPrice = async function({ commit }) {
+    try {
+        const response = awa
