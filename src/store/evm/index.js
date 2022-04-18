3b5705a83f2e72789af5bac039b39b25f13392d0
unfollowedDev <92226269+unfollowedDev@users.noreply.github.com>
2022-04-18 16:05:55 +0000

    addon

--- src/store/evm/index.js
+++ src/store/evm/index.js
@@ -4,4 +4,14 @@ import * as mutations from 'src/store/evm/mutations';
 import * as actions from 'src/store/evm/actions';
 
 // TODO: maybe this better...
-//   preloading the file so we don't 
+//   preloading the file so we don't have multiple components asking for contract and all triggering the token-list
+//   download at once
+//actions.getContract({}, {address: '0xdead'});
+
+export default {
+    namespaced: true,
+    state,
+    getters,
+    mutations,
+    actions,
+};
---
+++ src/store/evm/state.js
@@ -0,0 +1,6 @@
+export default function() {
+    return {
+        tlosPrice: 0.0,
+        gasPrice: 0.0,
+        latestBlock : 0,
+    
