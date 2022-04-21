50db31ba810c18043a97420a11e9356ace2cfdc4
unfollowedDev <92226269+unfollowedDev@users.noreply.github.com>
2022-04-21 11:18:29 +0000

    First cut

--- src/store/index.js
+++ src/store/index.js
@@ -7,4 +7,21 @@ import { store } from 'quasar/wrappers';
 
 /*
  * If not building with SSR mode, you can
- * directly expor
+ * directly export the Store instantiation
+ */
+
+export default store(function (/* { ssrContext } */) {
+    const Store = createStore({
+        modules: {
+            general,
+            login,
+            evm,
+        },
+
+        // enable strict mode (adds overhead!)
+        // for dev mode only
+        strict: process.env.DEV,
+    });
+
+    return Store;
+});
---
+++ src/store/login/actions.js
@@ -0,0 +1,2 @@
+export const login = async function(
+    { c
