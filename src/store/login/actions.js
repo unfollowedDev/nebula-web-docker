7fcdf52165527f8f63e4493d96a5c4fbe1956b27
unfollowedDev <92226269+unfollowedDev@users.noreply.github.com>
2022-04-21 15:28:20 +0000

    Updated pl translation

--- src/store/login/actions.js
+++ src/store/login/actions.js
@@ -1,2 +1,12 @@
 export const login = async function(
-    { c
+    { commit, dispatch },
+    { idx, account },
+) {
+    const authenticator = this.$ual.authenticators[idx];
+    try {
+        commit('setLoadingWallet', authenticator.getStyle().text);
+        await authenticator.init();
+        if (!account) {
+            const requestAccount = await authenticator.shouldRequestAccountName();
+            if (requestAccount) {
+                await dispatch('fetchAvailab
