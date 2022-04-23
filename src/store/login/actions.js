99f50f9094d3af90f7e73082f3376b2523bb7bf9
unfollowedDev <92226269+unfollowedDev@users.noreply.github.com>
2022-04-23 10:42:24 +0000

    npm test

--- src/store/login/actions.js
+++ src/store/login/actions.js
@@ -9,4 +9,15 @@ export const login = async function(
         if (!account) {
             const requestAccount = await authenticator.shouldRequestAccountName();
             if (requestAccount) {
-                await dispatch('fetchAvailab
+                await dispatch('fetchAvailableAccounts', idx);
+                commit('setRequestAccount', true);
+                return;
+            }
+        }
+        const users = await authenticator.login(account);
+        if (users.length) {
+            const account = users[0];
+            const accountName = await account.getAccountName();
+            this.$ualUser = account;
+            this.$type = 'ual';
+            commit('s
