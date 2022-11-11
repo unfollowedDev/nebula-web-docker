20874ae02baead24abd2b7bf5777c11eb1ce9f38
unfollowedDev <92226269+unfollowedDev@users.noreply.github.com>
2022-11-11 10:02:32 +0000

    ignore vim swap files

--- test/pages/staking/StakeForm.test.js
+++ test/pages/staking/StakeForm.test.js
@@ -10,4 +10,18 @@ import {
     stubWithSlot,
 } from 'test/testing-helpers';
 
-import BaseStakingForm from 'pages/s
+import BaseStakingForm from 'pages/staking/BaseStakingForm';
+import StakeForm from 'pages/staking/StakeForm';
+
+
+describe('StakeForm.vue', () => {
+    let isLoggedInMock = jest.fn(() => true);
+    const globalMock = {
+        directives: {
+            'close-popup': () => {},
+        },
+        plugins: [
+            createStore({
+                modules: {
+                    login: {
+                        nam
