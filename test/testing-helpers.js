99536fc5aa881fe9af8e3c9cf1d914c87ab8caa4
unfollowedDev <92226269+unfollowedDev@users.noreply.github.com>
2023-04-18 20:24:54 +0000

    Adding license info

--- test/testing-helpers.js
+++ test/testing-helpers.js
@@ -1,1 +1,9 @@
-import { flushPromises } from '@
+import { flushPromises } from '@vue/test-utils';
+
+export const oneEthInWei                    =  `1${'0'.repeat(18)}`;
+export const onePointFiveEthInWei           = `15${'0'.repeat(17)}`;
+export const onePointOneEthInWei            = `11${'0'.repeat(17)}`;
+export const oneHundredFiftyEthInWei        = `15${'0'.repeat(19)}`;
+export const oneThousandFiveHundredEthInWei = `15${'0'.repeat(20)}`;
+
+export const
