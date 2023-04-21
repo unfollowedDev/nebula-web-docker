faa24767b36cbdd86bb46a30fd3bf3661ca8f83d
unfollowedDev <92226269+unfollowedDev@users.noreply.github.com>
2023-04-21 06:57:43 +0000

    sdd

--- test/testing-helpers.js
+++ test/testing-helpers.js
@@ -6,4 +6,17 @@ export const onePointOneEthInWei            = `11${'0'.repeat(17)}`;
 export const oneHundredFiftyEthInWei        = `15${'0'.repeat(19)}`;
 export const oneThousandFiveHundredEthInWei = `15${'0'.repeat(20)}`;
 
-export const
+export const flushTimersAndPromises = async () => {
+    jest.runAllTimers();
+    await flushPromises();
+};
+
+/**
+ * Creates a stub component with an unnamed slot and arbitrary named slots
+ * For use in mount/shallowMount global options, e.g.
+ *
+ * const wrapper = shallowMount(SomeComponent, {
+ *     global: {
+ *         stubs: {
+ *             'q-banner': stubWithSlot('q-banner', ['action']),
+ *             'q-card': stubWithSlot('q
